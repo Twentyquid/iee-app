468566f524fccb46c02c7a218a1d0e4e6623987d
twentyquid <negativeradiations@gmail.com>
2024-07-25 22:03:19 +0530

    fixed manifest

--- vite.config.js
+++ vite.config.js
@@ -7,32 +7,31 @@ const manifestForPlugIn = {
   registerType:'prompt',
   includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
   manifest:{
-    name:"React-vite-app",
+    name:"IEEE APP",
     short_name:"react-vite-app",
     description:"I am a simple vite app",
     icons:[{
-      src: '/android-chrome-192x192.png',
+      src: '/android-chrome-192-192.png',
       sizes:'192x192',
       type:'image/png',
-      purpose:'favicon'
+      
     },
     {
-      src:'/android-chrome-512x512.png',
+      src:'/android-chrome-512-512.png',
       sizes:'512x512',
       type:'image/png',
-      purpose:'favicon'
+      
     },
     {
       src: '/apple-touch-icon.png',
       sizes:'180x180',
       type:'image/png',
-      purpose:'apple touch icon',
     },
     {
       src: '/maskable_icon.png',
       sizes:'512x512',
       type:'image/png',
-      purpose:'any maskable',
+
     }
   ],
   theme_color:'#171717',
