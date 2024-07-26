49e992f6de72ab9026b26eb49a0c5cacb1bf798e
twentyquid <negativeradiations@gmail.com>
2024-07-26 11:05:44 +0530

    added custom install prompt

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "@/components/ui/button";
 import { BrowserRouter } from "react-router-dom";
 import AuthWrapper from "./auth/AuthWrapper";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [promptevent, setPromptevent] = useState({});
+
+  const handler = (e) => {
+    e.preventDefault();
+    setPromptevent((prev) => e);
+    console.log(e);
+    setShowModal(true);
+    console.log("install triggered");
+
+    // setSupportsPWA(true);
+    // setPromptInstall(e);
+  };
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", handler);
+  }, []);
 
   return (
     <>
       <BrowserRouter>
         <div>
           <div className="max-w-[640px] h-screen bg-slate-50 mx-auto">
+            {showModal ? (
+              <div>
+                <button
+                  onClick={() => {
+                    promptevent.prompt();
+                  }}
+                >
+                  install App
+                </button>
+              </div>
+            ) : null}
             <AuthWrapper />
           </div>
         </div>
